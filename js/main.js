4a8f166aded9fb7ce429506d52070f9d8550f23f
dcoder2k19 <49821566+dcoder2k19@users.noreply.github.com>
2019-05-04 02:28:13 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$('.header-toggle').on("click", function() {
         $('.header-menu').slideToggle(400, function() {
             if($(this).css('display') == 'none'){
@@ -6,4 +7,44 @@ $(document).ready(function() {
             }
         });
     });
+
+    $('.scroll').click( function() {
+	var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+        }
+	    return false;
+    });
+
+    $(window).scroll(function() {
+    	if($(document).scrollTop() > 700) {
+    		$('.arrow-top').css('display', 'flex');
+    	} else {
+    		$('.arrow-top').css('display', 'none');
+    	}
+    });
+	$("#submit").on("click", function() {
+		var name = $("#name").val();
+		var email = $("#email").val();
+		var message = $("#message").val();
+		if(name != 0 && email != 0 && message != 0){
+			$.ajax({
+				url: "https://the-darks.000webhostapp.com/form.php",
+				type: "post",
+				data: {
+					"name": name,
+					"email": email,
+					"message": message
+				},
+				success: function(data) {
+					$("#msg").html('<div class="green">Succes!</div>');
+					$("#name").val('');
+					$("#email").val('');
+					$("#message").val('');
+				}
+			});
+		}else{
+			$("#msg").html('<div class="red">Fill in all the fields</div>');
+		}
+	});
 });
